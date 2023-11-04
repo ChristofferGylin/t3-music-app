2a11937fb0c37b7865626af4e995b97ade25f6a4
Christoffer Gylin <christoffer@christoffergylin.com>
2023-11-04 23:49:13 +0100

    Updated favicon and added Head with title tag in Wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,8 +1,15 @@
 import { PropsWithChildren } from "react";
-import Header from "./Header";
+import Head from "next/head";
 
 const Wrapper = ({ children }: PropsWithChildren) => {
-  return <div className="h-[calc(100vh)] w-full bg-slate-700">{children}</div>;
+  return (
+    <div className="h-[calc(100vh)] w-full bg-slate-700">
+      <Head>
+        <title>PlinkPlonk - Make music in your browser!</title>
+      </Head>
+      {children}
+    </div>
+  );
 };
 
 export default Wrapper;
