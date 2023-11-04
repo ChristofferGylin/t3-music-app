9498795108385db3e2e5aaa13b8567255a82e7f7
Christoffer Gylin <christoffer@christoffergylin.com>
2023-11-04 16:22:19 +0100

    Created condiotional back button, made header into grid, and updated IconButton with prop for alignment

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,18 +3,40 @@ import { type IconType } from "react-icons/lib/esm/iconBase";
 type IconButtonProps = {
   Icon: IconType;
   callback: () => void;
-  state: boolean;
+  state?: boolean;
+  align?: "left" | "right" | "center";
 };
 
-const IconButton = ({ Icon, callback, state }: IconButtonProps) => {
+const IconButton = ({
+  Icon,
+  callback,
+  state = false,
+  align,
+}: IconButtonProps) => {
   let iconColor = "fill-slate-300 hover:fill-slate-200";
+
+  let aligntment = "";
+
+  if (align) {
+    switch (align) {
+      case "left":
+        aligntment = "justify-self-start";
+        break;
+      case "right":
+        aligntment = "justify-self-end";
+        break;
+      case "left":
+        aligntment = "justify-self-center";
+        break;
+    }
+  }
 
   if (state) {
     iconColor = "fill-green-400";
   }
 
   return (
-    <button className="px-4 py-2" onClick={callback}>
+    <button className={`px-4 py-2 ${aligntment}`} onClick={callback}>
       <Icon className={`text-2xl ${iconColor}`} />
     </button>
   );
