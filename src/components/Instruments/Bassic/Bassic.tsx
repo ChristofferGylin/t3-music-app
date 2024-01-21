1de0d0711787de535f5fcdf9c6caa74bf8e89d49
Christoffer Gylin <christoffer@christoffergylin.com>
2024-01-21 17:04:23 +0100

    Created mobile view with tabs

--- src/components/Instruments/Bassic/Bassic.tsx
+++ src/components/Instruments/Bassic/Bassic.tsx
@@ -1,11 +1,12 @@
 import { type InstrumentType } from "~/types/InstrumentType";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext, type ContextType } from "~/context";
 import { type InstrumentStateBassicType } from "~/types/InstrumentStateType";
 import LFO from "./ParamGroups/LFO";
 import OSC from "./ParamGroups/OSC";
 import Filter from "./ParamGroups/Filter";
 import Envelope from "./ParamGroups/Envelope";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export const paramGroupClasses =
   "flex flex-col gap-2 h-full w-full items-center text-center justify-center rounded-xl border border-slate-600 p-1 sm:p-2 md:p-2 lg:p-3";
@@ -23,14 +24,49 @@ const Bassic = ({
 }) => {
   const { instrumentsState } = useContext(AppContext)! as ContextType;
   const state = instrumentsState[instrumentIndex] as InstrumentStateBassicType;
+  const [mobileTab, setMobileTab] = useState(1);
   if (instrument.modelName === "Bassic" && state !== undefined) {
     return (
-      <div className="grid grid-cols-[2fr_4fr_5fr_4fr] justify-center gap-1 rounded-xl border border-slate-700/70 bg-slate-700/50 p-0.5 shadow-lg sm:gap-2 sm:p-1 md:gap-2 md:p-2 lg:gap-3 lg:p-3">
-        <LFO instrumentIndex={instrumentIndex} />
-        <OSC instrumentIndex={instrumentIndex} />
-        <Filter instrumentIndex={instrumentIndex} />
-        <Envelope instrumentIndex={instrumentIndex} />
-      </div>
+      <>
+        <div className="min-w-80 min-h-44 hidden grid-cols-[2fr_4fr_5fr_4fr] justify-center gap-1 rounded-xl border border-slate-700/70 bg-slate-700/50 p-0.5 shadow-lg xs:grid sm:gap-2 sm:p-1 md:gap-2 md:p-2 lg:gap-3 lg:p-3">
+          <LFO instrumentIndex={instrumentIndex} />
+          <OSC instrumentIndex={instrumentIndex} />
+          <Filter instrumentIndex={instrumentIndex} />
+          <Envelope instrumentIndex={instrumentIndex} />
+        </div>
+        <div className="flex h-48 w-80 flex-col justify-start gap-1 rounded-xl border border-slate-700/70 bg-slate-700/50 p-0.5 shadow-lg xs:hidden">
+          <div className="grid h-4 grid-cols-3 border-b border-slate-500">
+            <div className="flex items-center justify-start text-[10px]">
+              <button
+                onClick={() => {
+                  setMobileTab(1);
+                }}
+              >
+                <IoIosArrowBack className="hover:text-white" />
+              </button>
+              <button
+                onClick={() => {
+                  setMobileTab(2);
+                }}
+              >
+                <IoIosArrowForward className="hover:text-white" />
+              </button>
+            </div>
+          </div>
+          {mobileTab === 1 && (
+            <div className="grid h-full grid-cols-[2fr_4fr_3fr] items-start justify-start gap-1 rounded-xl border border-slate-700/70 bg-slate-700/50 p-0.5 shadow-lg">
+              <LFO instrumentIndex={instrumentIndex} />
+              <OSC instrumentIndex={instrumentIndex} />
+            </div>
+          )}
+          {mobileTab === 2 && (
+            <div className="grid grid-cols-[5fr_4fr] justify-center gap-1 rounded-xl border border-slate-700/70 bg-slate-700/50 p-0.5 shadow-lg">
+              <Filter instrumentIndex={instrumentIndex} />
+              <Envelope instrumentIndex={instrumentIndex} />
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 
